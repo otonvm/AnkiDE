77bf9ac152f8c8974c60777b11e31cc864b537c2
Oton Mahnič <otonvm@gmail.com>
2016-08-27 12:54:25 +0200

    Rewrite of parser

--- src/wiktionary_parser.py
+++ src/wiktionary_parser.py
@@ -2,21 +2,18 @@
 
 import re
 import attr
-import requests
-
-
-class Continue(Exception):
-    pass
+from isplit import isplit
 
 
 @attr.s
 class WiktionaryParser:
     _markup = attr.ib(validator=attr.validators.instance_of(str))
-    _data = attr.ib(init=False, default=attr.Factory(dict))
+    _word_data = attr.ib(init=False, default=attr.Factory(dict))
     _block_sequence_num = attr.ib(init=False)
-    _lines = attr.ib(init=False)
+    _markup_by_line = attr.ib(init=False, default=None)
 
     def _clean_line(self, line):
+        line = line.strip()
         # remove bold tags:
         line = re.sub(r"'''(.+)'''", r"\1", line)
         # remove italics:
@@ -36,184 +33,156 @@ class WiktionaryParser:
 
         return line
 
-    def basic_form(self):
-        print(self._data["conjugated"])
-        if self._data["conjugated"]:
-            for line in self._lines:
-                print(self._lines)
-                match = re.match(r"^\{\{.*?\|(\w+)\}\}$", line)
-                if match:
-                    self._data["basic form"] = match.group(1)
+    def _ready_markup_generator(self):
+        self._markup_by_line = (self._clean_line(line) for line in
+                                isplit(self._markup, "\n") if line.strip())
 
-    def _is_conjugated(self, line):
-        if "Konjugierte Form" in line:
-            self._data["conjugated"] = True
+    def _get_matches(self, pattern):
+        self._ready_markup_generator()
 
-    def _find_alternative_word(self, line):
-        match = re.match(r"^\{\{Siehe\sauch\|'''\[\[([\w]+)\]\]'''\}\}$", line)
-        if match:
-            self._data["alternative"] = match.group(1)
-            raise Continue
-
-    def _find_type_of_word(self, line):
-        match = re.match(r"^===\s\{\{Wortart\|(\w+)\|Deutsch\}\}", line)
-        if match:
-            self._data["type"] = match.group(1)
-            raise Continue
-
-    def _find_overview_line(self, line):
-        match = re.match(r"^\|([a-zA-zäöü\s*,]+)=(\w+)$", line)
-
-        if match:
-            return match.groups()
-        else:
-            raise Continue
-
-    def _find_word_overview(self, line):
-        if re.match(r"^\{\{Deutsch\s\w+\sÜbersicht$", line):
-            self._data["overview"] = {}
-
-            # loop until next block:
-            for line in self._lines:
-                if line.startswith("{{"):
-                    break
-
-                line = self._clean_line(line)
-
-                try:
-                    label, text = self._find_overview_line(line)
-                except Continue:
-                    continue
-
-                self._data["overview"][label] = text
-
-            raise Continue
-
-    def _find_audio(self, line):
-        match = re.match(r"^:\{\{Hörbeispiele\}\} \{\{Audio\|([\w\-.]+)[}|\w]+", line)
-        if match:
-            self._data["audio"] = match.group(1)
-            raise Continue
-
-    def _find_notes_in_meaning_line(self, line):
-        match = re.match(r"^\{\{K\|([\w.|]+)\}\}\s(.*)$", line)
-        if match:
-            notes, rest = match.groups()
-            # transform | to ,
-            notes = notes.replace("|", ", ")
-            return notes, rest
-        else:
-            return None, line
-
-    def _find_meaning_line(self, line):
-        match = re.match(r"^:\[\d+\]\s(.*)$", line)
-        if match:
-            self._block_sequence_num += 1
-            text = match.group(1)
-            notes, text = self._find_notes_in_meaning_line(text)
-            return self._block_sequence_num, notes, text
-        else:
-            raise Continue
-
-    def _find_word_meanings(self, line):
-        if line == "{{Bedeutungen}}":
-            self._data["meanings"] = {}
-            self._block_sequence_num = 0
-
-            # loop until next block:
-            for line in self._lines:
-                if line.startswith("{{"):
-                    break
-
-                line = self._clean_line(line)
-
-                try:
-                    number, notes, text = self._find_meaning_line(line)
-                except Continue:
-                    continue
-
-                self._data["meanings"][int(number)] = (notes, text)
-
-            raise Continue
-
-    def _find_example_line(self, line):
-        match = re.match(r"^:\[[\s\d,\]]+(.*)$", line)
-        if match:
-            self._block_sequence_num += 1
-            return self._block_sequence_num, match.group(1)
-        else:
-            raise Continue
-
-    def _find_word_examples(self, line):
-        if line == "{{Beispiele}}":
-            self._data["examples"] = {}
-            self._block_sequence_num = 0
-
-            # loop until next block:
-            for line in self._lines:
-                if line.startswith("{{"):
-                    break
-
-                line = self._clean_line(line)
-
-                try:
-                    number, text = self._find_example_line(line)
-                except Continue:
-                    continue
-
-                self._data["examples"][int(number)] = text
-
-            raise Continue
-
-    def _find_translation_line(self, line):
-        match = re.match(r"^\*\{\{en\}\}.*?\|([\w\s']+)\}\}", line)
-        if match:
-            return match.group(1)
-        else:
-            raise Continue
-
-    def _find_word_translation(self, line):
-        if line == "==== Übersetzungen ====":
-            # loop until next block:
-            for line in self._lines:
-                if line.startswith("{{") and "Tabelle" not in line:
-                    break
-
-                line = self._clean_line(line)
-
-                try:
-                    text = self._find_translation_line(line)
-                except Continue:
-                    continue
-
-                self._data["translation"] = text
-
-            raise Continue
-
-    def parse(self):
-        self._lines = (line.strip() for line in self._markup.split("\n") if line.strip())
-
-        for line in self._lines:
-            line = self._clean_line(line)
+        for line in self._markup_by_line:
+            match = re.match(pattern, line)
 
             try:
-                self._is_conjugated(line)
-                self._find_alternative_word(line)
-                self._find_type_of_word(line)
-                self._find_word_overview(line)
-                self._find_audio(line)
-                self._find_word_meanings(line)
-                self._find_word_examples(line)
-                self._find_word_translation(line)
-            except Continue:
+                return match.group(1) if len(match.groups()) == 1 else match.groups()
+            except AttributeError:
                 continue
 
-    def data(self):
-        return self._data
+    def _get_matches_for_block(self, label, block_start_pattern, line_pattern):
+        self._ready_markup_generator()
 
-word = "ohrfeige"
-w = WiktionaryParser(requests.get(r"https://de.wiktionary.org/wiki/{}?action=raw".format(word)).text)
-w.parse()
-from pprint import pprint
-pprint(w.data())
-print(w.basic_form())
+        self._word_data[label] = {}
+
+        for line in self._markup_by_line:
+            if re.match(block_start_pattern, line):
+
+                for block_line in self._markup_by_line:
+                    if block_line.startswith("{{"):
+                        return
+
+                    match = re.match(line_pattern, block_line)
+
+                    try:
+                        self._word_data[label][match.group(1)] = match.group(2)
+                    except AttributeError:
+                        continue
+
+    def _get_numbered_matches_for_block(self, label, block_start_pattern, line_pattern, notes_pattern=None):
+        self._ready_markup_generator()
+
+        self._word_data[label] = {}
+
+        number = 0
+
+        for line in self._markup_by_line:
+            if re.match(block_start_pattern, line):
+
+                for block_line in self._markup_by_line:
+                    if block_line.startswith("{{"):
+                        return
+
+                    number += 1
+
+                    line_match = re.match(line_pattern, block_line)
+                    if line_match:
+                        if notes_pattern is not None:
+                            notes_match = re.match(notes_pattern, line_match.group(1))
+
+                            if notes_match:
+                                notes, text = notes_match.groups()
+                                notes = notes.replace("|", ", ")
+
+                                self._word_data[label][number] = (notes, text)
+
+                            else:
+                                self._word_data[label][number] = (None, line_match.group(1))
+
+                        else:
+                            self._word_data[label][number] = line_match.group(1)
+
+    def word_type(self):
+        try:
+            self._word_data["type"]
+        except KeyError:
+            self._word_data["type"] = self._get_matches(r"^===\s\{\{Wortart\|([\w\s]+)\|Deutsch\}\}")
+        finally:
+            return self._word_data["type"]
+
+    def is_conjugated(self):
+        return self._data["type"] == "Konjugierte Form"
+
+    def basic_form(self):
+        try:
+            self._word_data["basic form"]
+        except KeyError:
+            self._word_data["basic form"] = self._get_matches(r"^\{\{Grundformverweis Konj\|(\w+)\}\}")
+        finally:
+            return self._word_data["basic form"]
+
+    def alternative_word(self):
+        try:
+            self._word_data["alternative"]
+        except KeyError:
+            self._word_data["alternative"] = self._get_matches(r"^\{\{Siehe\sauch\|'''\[\[([\w]+)\]\]'''\}\}$")
+        finally:
+            return self._word_data["alternative"]
+
+    def overview(self):
+        try:
+            self._word_data["overview"]
+        except KeyError:
+            self._get_matches_for_block(
+                label="overview",
+                block_start_pattern=r"^\{\{Deutsch\s\w+\sÜbersicht$",
+                line_pattern=r"^\|([a-zA-zäöü\s*,]+)=(\w+)$",
+            )
+        finally:
+            return self._word_data["overview"]
+
+    def audio(self):
+        try:
+            self._word_data["audio"]
+        except KeyError:
+            self._word_data["audio"] = self._get_matches(r"^:\{\{Hörbeispiele\}\} \{\{Audio\|([\w\-.]+)[}|\w]+")
+        finally:
+            return self._word_data["audio"]
+
+    def meanings(self):
+        try:
+            self._word_data["meanings"]
+        except KeyError:
+            self._get_numbered_matches_for_block(
+                label="meanings",
+                block_start_pattern=r"^\{\{Bedeutungen\}\}$",
+                line_pattern=r"^:\[\d+\]\s(.*)$",
+                notes_pattern=r"^\{\{K\|([\w.|]+)\}\}\s(.*)$"
+            )
+        finally:
+            return self._word_data["meanings"]
+
+    def examples(self):
+        try:
+            self._word_data["examples"]
+        except KeyError:
+            self._get_numbered_matches_for_block(
+                label="examples",
+                block_start_pattern="^\{\{Beispiele\}\}$",
+                line_pattern=r"^:\[[\s\d,\]]+(.*)$"
+            )
+        finally:
+            return self._word_data["examples"]
+
+    def translation(self):
+        try:
+            self._word_data["translation"]
+        except KeyError:
+            self._get_matches_for_block(
+                label="translation",
+                block_start_pattern=r"^\{\{Ü-Tabelle\|Ü-links=$",
+                line_pattern=r"^\*\{\{(en)\}\}.*?\|([\w\s']+)\}\}",
+            )
+        finally:
+            return self._word_data["translation"]
+
+    def word_data(self):
+        return self._word_data
